740c6692aa20af8a40fec7ec9a50edf51069f07a
pojsem18 <57000273+pojsem18@users.noreply.github.com>
2020-06-12 12:27:47 +0200

    Add files via upload

--- 2ahme/ue13_felder/main.c
+++ 2ahme/ue13_felder/main.c
@@ -78,9 +78,95 @@ void eindimensionalesFeld() {
     printf("\n\n");
 }
 
+void print2DimFeld(double f[][3], int anzahl) {
+    printf("print2DimFeld:\n");
+    for (int i = 0; i < anzahl; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("feld[%d][%d] %p => %f\n",
+                   i, j, &f[i][j], f[i][j]);
+        }
+    }
+    printf("\n\n");
+}
+
+void zweidimensionaleFelder() {
+    // xx-Achse & y-Achse
+    // 4 * 3 = 12 Elemente -> 12 * 8 = 96
+    double f[4][3] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+    printf("\n\n2 Dimensionen:\n");
+    printf("sizeof(f) = %d Bytes\n", (int)sizeof(f));
+    printf("f[0][0] %p => %f\n", &f[0][0], f[0][0]);
+    printf("f[0][1] %p => %f\n", &f[0][1], f[0][1]);
+    printf("f[0][2] %p => %f\n", &f[0][2], f[0][2]);
+    printf("f[1][0] %p => %f\n", &f[1][0], f[1][0]);
+    printf("f[1][1] %p => %f\n", &f[1][1], f[1][1]);
+    printf("f[1][2] %p => %f\n", &f[1][2], f[1][2]);
+    printf("f[2][0] %p => %f\n", &f[2][0], f[2][0]);
+    printf("f[2][1] %p => %f\n", &f[2][1], f[2][1]);
+    printf("f[2][2] %p => %f\n", &f[2][2], f[2][2]);
+    printf("f[3][0] %p => %f\n", &f[3][0], f[3][0]);
+    printf("f[3][1] %p => %f\n", &f[3][1], f[3][1]);
+    printf("f[3][2] %p => %f\n", &f[3][2], f[3][2]);
+
+    // f = { { 1, 2, 3 } }; // nicht moeglich, nur bei Initialisierung erlaubt
+
+    f[2][0] = 7;
+    f[2][1] = 8;
+    f[2][2] = 9;
+    f[2][3] = 10;
+
+    print2DimFeld(f, 4);
+}
+
+void print3DimFeld(double x[][3][4], int anzahl) {
+    printf("print3DimFeld:\n");
+    for (int i = 0; i < anzahl; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                printf("x[%d][%d][%d] %p => %f\n",
+                       i, j, k, &x[i][j][k], x[i][j][k]);
+            }
+        }
+    }
+    printf("\n\n");
+}
+
+void dreidimensionalesFeld() {
+
+    double f[2][3][4] =
+        {
+            {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
+            }, {
+                { 13, 14, 15, 16 },
+                { 17, 18, 19, 20 },
+                { 21, 22, 23, 24 }
+            }
+        };
+
+    printf("\n\n3 Dimensionen:\n");
+    printf("sizeof(f) = %d Bytes\n", (int)sizeof(f));
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                printf("feld[%d][%d] %p => %f\n",
+                   i, j, k, &f[i][j][k], f[i][j][k]);
+            }
+        }
+    }
+    printf("\n\n");
+    
+    print3DimFeld(f, 2);
+    
+}
+
 int main() {
 
-    eindimensionalesFeld();
-
+    //eindimensionalesFeld();
+    zweidimensionaleFelder();
     return 0;
 }
